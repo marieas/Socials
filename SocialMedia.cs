6adbed10e941e0499611e3760c30acbf13e3b9d4
marie <marie@getacademy.no>
2024-05-29 14:01:41 +0200

    vhv

---
+++ SocialMedia.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SovialMedia
+{
+    internal class SocialMedia
+    {
+        List<UserProfile> Users {  get; set; }
+        UserProfile CurrentUser { get; set; }
+        public SocialMedia()
+        {
+            Users = new List<UserProfile>()
+            {
+                new UserProfile("Bjarne Kråkerød", new List<string>{"Gaming","TV"}),
+                new UserProfile("Kåre Bjarteland", new List<string>{"Hiking","Eating"}),
+                new UserProfile("Kåre HigeLand", new List<string>{"Hiking","Eating"}),
+                new UserProfile("Kåre SuperLand", new List<string>{"Hiking","Eating"}),
+                new UserProfile("Kåre TommelLand", new List<string>{"Hiking","Eating"})
+            };
+            CurrentUser = Users[0];
+            MainMenu();
+        }
+
+        private void MainMenu()
+        {
+            while (true)
+            {
+                Console.WriteLine($"Welcome to social media {CurrentUser.Name}");
+                Console.WriteLine("What do you want to do?");
+                Console.WriteLine("1. View friends");
+                Console.WriteLine("2. Add friend");
+                Console.WriteLine("3. Remove friend");
+                Console.WriteLine("4. Exit");
+
+                var userInput = Console.ReadLine();
+                switch(userInput)
+                {
+                    case "1":
+                        CurrentUser.PrintFriends();
+                        break;
+                    case "2":
+                        AddFriendMenu();
+                        break;
+                    case "3":
+                        RemoveFriendMenu();
+                        break;
+                    case "4":
+                        System.Environment.Exit(0);
+                        break;
+                }
+            }
+            void AddFriendMenu()
+            {
+                Console.WriteLine("Who do you want to add?");
+                printUsersOnSM();
+                var userName = Console.ReadLine();
+                var userToAdd = Users.First(user => user.Name.Contains(userName));
+                CurrentUser.AddFriend(userToAdd);
+            }
+            void RemoveFriendMenu()
+            {
+                Console.WriteLine("Who do you want to remove?");
+                CurrentUser.PrintFriends();
+                var userName = Console.ReadLine();               
+                CurrentUser.RemoveFriend(userName);
+            }
+            void printUsersOnSM()
+            {
+                foreach (var user in Users)
+                {
+                    user.PrintProfileInfo();
+                }
+            }
+        }
+        
+    }
+}
---
+++ UserProfile.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO.Pipes;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SovialMedia
+{
+    internal class UserProfile
+    {
+        public string Name { get; set; }
+        public List<string> Hobbies { get; set; }
+        public List<UserProfile> FriendsList { get; set; }
+
+        public UserProfile(string name, List<string> hobbies)
+        {
+            Name = name;
+            Hobbies = hobbies;
+            FriendsList = new List<UserProfile>();
+        }
+        public void PrintProfileInfo()
+        {
+            Console.WriteLine($"Name: {Name}");
+            Console.WriteLine($"hobbies:");
+            PrintHobbies();
+        }
+
+        public void AddHobby(string hobby)
+        {
+            Hobbies.Add(hobby);
+        }
+        public void AddFriend(UserProfile friend)
+        {
+            FriendsList.Add(friend);
+        }
+        public void RemoveFriend(string name)
+        {
+            if(FriendsList.Count == 0) { 
+                Console.WriteLine("No friend to remove!");
+                return;
+            }
+            var userToDelete = FriendsList.First(friend => friend.Name.Contains(name));
+            if(userToDelete != null)
+            {
+                FriendsList.Remove(userToDelete);
+            }
+            else
+            {
+                Console.WriteLine($"{Name} does not have a friend called {name}");
+            }
+            
+        }
+        private void PrintHobbies()
+        {
+            Console.WriteLine($"{Name} has current hobbies:");
+            foreach (var hobby in Hobbies) {
+                Console.WriteLine(hobby);
+            }
+        }
+        public void PrintFriends()
+        {
+            if (FriendsList.Count == 0) { 
+                Console.WriteLine("you dont have any friends :(");
+                Console.WriteLine("check out users to add!");
+                return;
+            }
+            foreach (var friend in FriendsList)
+            {                
+                friend.PrintProfileInfo();
+            }
+        }
+    }
+}
